dc983aafdc8392327d907ecaed6da00da42a5f04
Jose francisco Arredondo amador <jarredon@c1r10s3.42malaga.com>
2022-06-15 11:10:23 +0200

    Fix atof

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: jarredon <jarredon@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 15:02:57 by jarredon          #+#    #+#             */
-/*   Updated: 2022/05/11 15:05:53 by jarredon         ###   ########.fr       */
+/*   Updated: 2022/06/15 11:35:40 by jarredon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,58 +41,28 @@ static int	ft_atoi(const char *str)
 	return ((int) num);
 }
 
-static unsigned int	ft_strlen(const char *s)
-{
-	unsigned int	len;
-
-	len = 0;
-	while (*s++)
-		len++;
-	return (len);
-}
-
-static char	*ft_strdup(const char *s1)
-{
-	unsigned int	len;
-	char			*copy;
-	char			*tmp;
-
-	len = ft_strlen(s1);
-	copy = (char *)malloc(sizeof(char) * len + 1);
-	if (copy == 0)
-		return (0);
-	tmp = copy;
-	while (*s1)
-		*tmp++ = *s1++;
-	*tmp = '\0';
-	return (copy);
-}
-
 double	ft_atof(char *str)
 {
-	char	*copy;
 	int		i;
 	double	ret;
-	char	*deci;
 	double	n_deci;
 
-	copy = ft_strdup(str);
 	i = 0;
-	while (copy[i] && copy[i] != '.')
+	while (str[i] && str[i] != '.')
 		i++;
-	if (!copy[i])
+	if (!str[i])
 	{
-		ret = (double)ft_atoi(copy);
-		free(copy);
+		ret = (double)ft_atoi(str);
 		return (ret);
 	}
-	deci = &copy[i + 1];
-	copy[i] = '\0';
-	ret = (double)ft_atoi(copy);
-	n_deci = (double)ft_atoi(deci);
+	ret = (double)ft_atoi(str);
+	n_deci = (double)ft_atoi(&str[i + 1]);
 	while (n_deci >= 1)
 		n_deci /= 10;
-	free(copy);
+	while (str[++i] == '0')
+		n_deci /= 10;
+	if (str[0] == '-')
+		return (ret - n_deci);
 	return (ret + n_deci);
 }
 
